f1d91a03b5fce643fb593da6b8763532cbfd4a2c
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-06-22 11:59:20 -0500

    complete the assignment

---
+++ Lessons/Lesson01/Assignment/assignment.js
@@ -0,0 +1,12 @@
+const express=require("express");
+const path=require("path");
+require("dotenv").config();
+
+const app=express();
+
+app.listen(process.env.PORT,function(){
+console.log("Listening to port",process.env.PORT);
+})
+
+app.use(express.static(path.join(__dirname,process.env.EXPRESS_PUBLIC_FOLDER)));
+
