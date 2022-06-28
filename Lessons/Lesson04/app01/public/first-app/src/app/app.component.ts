dfc4ab8efebf2bb2394819d3f1d80b9852b201b0
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-06-28 12:04:15 -0500

    complete lesson 4

--- Lessons/Lesson04/app01/public/first-app/src/app/app.component.ts
+++ Lessons/Lesson04/app01/public/first-app/src/app/app.component.ts
@@ -8,4 +8,18 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'first-app';
+  myName = "Sauravi";
+  people = [{ name: "Jack", gpa: 3.0, course: "MWA" },
+  { name: "Jill", gpa: 3.5, course: "MPP" }]
+
+  salary: number=123.4567;
+
+  today= new Date();
+
+  student={ name: "Jack", gpa: 3.0};
+
+  onButtonClick(): void {
+    this.title = "New title";
+  }
+
 }
