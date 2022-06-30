81da1cb58614d6fb5180b33792ed128113e90d34
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-06-30 11:57:52 -0500

    add form initialization

--- FullStack/Frontend/public/php/src/app/login/login.component.ts
+++ FullStack/Frontend/public/php/src/app/login/login.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+
+class Credentails{
+  username:string;
+  password:string;
+  constructor(username:string,password:string){
+this.username=username;
+this.password=password;
+  }
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,6 +21,8 @@ export class LoginComponent implements OnInit {
   @ViewChild("loginForm")
   loginForm!:NgForm;
 
+  user:Credentails=new Credentails("jack","jill");
+
   constructor() { }
 
   ngOnInit(): void {
@@ -18,7 +30,7 @@ export class LoginComponent implements OnInit {
 
   onLogin():void{
       console.log("Login Clicked")
-      console.log(this.loginForm.value);
+      console.log(this.user.username," ",this.user.password );
   }
 
 }
