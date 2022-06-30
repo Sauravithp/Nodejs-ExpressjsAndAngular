9f310b8d5efd64c141cbc84ed5feb1ba1086b650
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-06-29 23:13:38 -0500

    complete parentchild practice

---
+++ FullStack/Frontend/public/parentchild/src/app/child/child.component.ts
@@ -0,0 +1,41 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  @Input()
+  childX!:number;
+
+  @Input()
+  childY!:number;
+  
+  childZ!:number
+
+  @Output()
+  addEvent:EventEmitter<number>=new EventEmitter<number>;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAdd():void{
+    this.childZ=this.childX+this.childY;
+
+    this.childX=0;
+    this.childY=0;
+    this.addEvent.emit(this.childZ);
+  }
+
+  onSub():void{
+
+    this.childZ=this.childX-this.childY;
+    this.childX=0;
+    this.childY=0;
+  }
+
+}
---
+++ FullStack/Frontend/public/parentchild/src/app/parent/parent.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css']
+})
+export class ParentComponent implements OnInit {
+
+  parentX=5;
+  parentY=5;
+  parentZ!:number;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  updateZ(parentz:number):void{
+    this.parentZ=parentz;
+  }
+
+}
