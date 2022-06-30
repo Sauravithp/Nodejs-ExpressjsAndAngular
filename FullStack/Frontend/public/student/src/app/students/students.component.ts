4234808dd48fe19b6b6d12182a98f56eba2d72af
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-06-30 09:39:56 -0500

    completye

--- FullStack/Frontend/public/student/src/app/students/students.component.ts
+++ FullStack/Frontend/public/student/src/app/students/students.component.ts
@@ -2,6 +2,40 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute} from '@angular/router';
 import { StudentDataService } from '../student-data.service';
 
+
+
+export class Student{
+  #_id!:string;
+  #_name!:String;
+  #_gpa!: number;
+
+  constructor(){}
+
+  get gpa(){
+    return this.#_gpa;
+  }
+
+  get _id(){
+    return this.#_id;
+  }
+
+  get _name(){
+    return this.#_name
+  }
+
+  set gpa(gpa:number){
+       this.#_gpa= gpa;
+  }
+
+  set name(name:string){
+      this.#_name=name;
+  }
+
+  set _id(_id:string){
+      this.#_id=_id
+  }
+}
+
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
@@ -9,19 +43,21 @@ import { StudentDataService } from '../student-data.service';
 })
 export class StudentsComponent implements OnInit {
 
-  students:any[]=[];
+  students:Student[]=[];
 
   constructor(private _service: StudentDataService, private _route:ActivatedRoute) { }
 
   ngOnInit(): void {
-
     this._service.getStudents().subscribe(response=>{
       console.log("inside get")
       const students=response;
       this.students=students;
       console.log("data: ",this.students);
     });
-
   }
+
+
+
+
 
 }
